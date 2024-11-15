b6f3b82f386058c0290f1b18029797c28b1b38cf
Laluboom <viseshakbari@gmail.com>
2024-11-15 23:48:32 +0000

    cleaning for errors found in explorecard

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,4 +1,5 @@
 'use client';
+
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import styles from '../styles';
@@ -7,8 +8,8 @@ import { fadeIn } from '../utils/motion';
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
     variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
-    className={`relative ${active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
-      } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+    className={`relative ${active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'}
+    flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
     onClick={() => handleClick(id)}
   >
     <img src={imgUrl} alt={title} className="absolute w-full h-full object-cover rounded-[24px]" />
@@ -20,7 +21,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
     ) : (
       <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
         <Link href="/pages/Standalone" legacyBehavior>
-          <a className={`${styles.flexCenter} w-[60px] h-[60px] glassmorphism mb-[16px] rounded-[12px]`}>
+          <a href="#" className={`${styles.flexCenter} w-[60px] h-[60px] glassmorphism mb-[16px] rounded-[12px]`}>
             <img src="/headset.svg" alt="headset" className="w-1/2 h-1/2 object-contain" />
           </a>
         </Link>
@@ -31,14 +32,6 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
       </div>
     )}
   </motion.div>
-
 );
 
 export default ExploreCard;
-
-
-{/* <Link href="/UIlayout" legacyBehavior>
-<a className={`${styles.flexCenter} w-[60px] h-[60px] glassmorphism mb-[16px] rounded-[12px]`}>
-  <img src="/headset.svg" alt="headset" className="w-1/2 h-1/2 object-contain" />
-</a>
-</Link> */}
--- next.config.js
+++ next.config.js
@@ -6,3 +6,8 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+// module.exports = {
+//   output: 'export',
+//   basePath: '/your-repo-name',
+// };
